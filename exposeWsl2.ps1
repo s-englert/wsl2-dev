4bc2a30898d0650cdadb8e546ffa0c751ff5121b
Stefan Englert <stefan@englert.net>
2021-11-23 20:11:59 +0100

    Update exposeWsl2.ps1

--- exposeWsl2.ps1
+++ exposeWsl2.ps1
@@ -1,26 +1,26 @@
-# Get the IPv4 from running Wsl2 Linux
-
 $remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
 $found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
 
 if( $found ){
   $remoteport = $matches[0];
+  echo "Expose ports of IP: $remoteport";
 } else{
-  echo "IP not found, is Wsl2 running?";
+  echo "The Script Exited, the ip address of WSL 2 cannot be found";
   exit;
 }
 
-# Ports
-# All Ports you will make available
+#[Ports]
+
+#All the ports you want to forward separated by coma
 $ports=@(80,443,3306);
 
-# Static IPv4
-# If you have a static IP on your Windows, you cat set it up here 0.0.0.0 is var.
+#[Static ip]
+#You can change the addr to your ip config to listen to a specific address
 $addr='0.0.0.0';
 
-# Delete existing rules on that ports, and create new with requested IPv4
 for( $i = 0; $i -lt $ports.length; $i++ ){
   $port = $ports[$i];
+  echo " - set port: $port";
   iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
   iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
 }
